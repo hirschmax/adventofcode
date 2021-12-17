2e18059417c650918143400165fc825d9d337c70
mhirsch <mhirsch@it-economics.de>
2021-12-17 17:01:09 +0100

    Changes by shilsmann

--- src/main/java/de/ite/adventofcode/sonar/SonarLogService.java
+++ src/main/java/de/ite/adventofcode/sonar/SonarLogService.java
@@ -5,7 +5,15 @@ import java.util.List;
 public class SonarLogService {
 
     public int computeNumberOfTimesDepthIncreasesFor(List<String> sonarLog) {
-        return -1;
+        int numberOfTimesDepthIncreases = 0;
+
+        for (int i = 1; i < sonarLog.size(); i++) {
+            if (Integer.parseInt(sonarLog.get(i)) > Integer.parseInt(sonarLog.get(i -1))) {
+                numberOfTimesDepthIncreases++;
+            }
+        }
+
+        return numberOfTimesDepthIncreases;
     }
 
 
